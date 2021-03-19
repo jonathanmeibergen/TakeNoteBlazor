1047e24b3c3b688717eaff349b09ade16bb69f86
Jonathan Meibergen <jonathanmeibergen@outlook.com>
2021-03-19 13:39:47 +0100

    Basic Master/Detail for notes functionality

---
+++ TakeNoteBlazor/Client/Services/IMasterDetailService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TakeNoteBlazor.Client.Services
+{
+    interface IMasterDetailService
+    {
+        event Action<Object> OnView;
+        event Action<Object> OnCreate;
+        event Action<Object> OnEdit;
+    }
+}
