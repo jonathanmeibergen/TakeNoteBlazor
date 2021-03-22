4a7c355a390ebf94c149eee251f1a6223b6cd296
Jonathan Meibergen <jonathanmeibergen@outlook.com>
2021-03-22 16:03:24 +0100

    Basic Identity Template is now implemented

--- TakeNoteBlazor/Server/Data/ApplicationDbContext.cs
+++ TakeNoteBlazor/Server/Data/ApplicationDbContext.cs
@@ -1,17 +1,23 @@
 ﻿using TakeNoteBlazor.Shared;
+using IdentityServer4.EntityFramework.Options;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TakeNoteBlazor.Server.Models;
 
 namespace TakeNoteBlazor.Server.Data
 {
-    public class ApplicationDbContext : DbContext
-    {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) :base(options)
+        public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
         {
+            public ApplicationDbContext(
+                DbContextOptions options,
+                IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
+            {
+            }
+            public DbSet<Note> Notes { get; set; }
         }
-        public DbSet<Note> Notes { get; set; }
-    }
 }
